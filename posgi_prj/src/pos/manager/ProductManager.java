42ea3245e9f48be4f53ab3616fbf7f4076d8e8af
nemo <twdolphin77@gmail.com>
2025-12-14 20:42:04 +0900

    Can't connect to any repository: https://github.com/lgj-0853/posgi_prj.git (https://github.com/lgj-0853/posgi_prj.git: git-receive-pack not permitted on 'https://github.com/lgj-0853/posgi_prj.git/')

--- posgi_prj/src/pos/manager/ProductManager.java
+++ posgi_prj/src/pos/manager/ProductManager.java
@@ -1,9 +1,66 @@
-package pos.manager; // 데이터 생성, 조회, 수정, 총액 계산 같은 데이터 조작 역할
+package pos.manager;
+import java.util.ArrayList;
+import java.util.List;
+import pos.model.Product; 		// Product 클래스를 사용하기 위한 import
 
-public class ProductManager { // 중앙 데이터 창고: 모든 상품 데이터를 **List**에 저장하고, 데이터의 추가/수정/삭제 등 핵심 로직을 오직 이 클래스만 수행하도록 합니다.
 
+public class ProductManager { 
+
+    // List<Product> 자료구조 선언
+    private List<Product> productList; 
+
+    
+    public ProductManager() {
+       
+        this.productList = new ArrayList<>();
+        
+       
+       SampleData();
+        
+        System.out.println("총 " + productList.size() + "개 상품 로드됨.");
+    }
+
+    
+    private void SampleData() {
+         
+        productList.add(new Product("001", "빼빼로", 1500, 10));
+        productList.add(new Product("002", "구구콘", 8000, 25));
+        productList.add(new Product("003", "포카칩", 2000, 50));
+        productList.add(new Product("004", "참치삼김",1000, 30));
+        productList.add(new Product("005", "왕뚜껑", 1800, 15));
+    }
+    
+   
+    public Product getProductByCode(String code) {
+        if (code == null || code.trim().isEmpty()) {
+            return null;
+        }
+
+        
+        for (Product product : productList) {
+            
+            if (product.getCode().equals(code)) {
+                return product; 
+            }
+        }
+        
+        return null; 
+    }
+    
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        
+        ProductManager manager = new ProductManager();
+        
+        System.out.println("--- 상품 검색 테스트 ---");
+        
+        Product gugucorn = manager.getProductByCode("002");
+        if (gugucorn != null) {
+            System.out.println("검색 성공 (002): " + gugucorn.getName() + ", 가격: " + gugucorn.getPrice());
+        } else {
+            System.out.println("검색 실패 (002): 상품을 찾을 수 없음.");
+        }
+        
 
 	}
 
--- posgi_prj/src/pos/model/Product.java
+++ posgi_prj/src/pos/model/Product.java
@@ -2,11 +2,75 @@ package pos.model; // 시스템에서 사용되는 순수한 데이터 객체들
 
 
 public class Product { // 상품 정보의 틀: 상품 하나의 데이터(코드, 이름, 가격, 재고)를 담기 위한 설계도.
+	
+		private String code;  
+		private String name; 
+		private int price;    
+		private int stock;    
+
+		//생성자
+		public Product() {
+			
+		}
+	    
+	    // 초기화
+		public Product(String code, String name, int price, int stock) {
+			this.code = code;
+			this.name = name;
+			this.price = price;
+			this.stock = stock;
+		}
+
+		
+		// Getter 및 Setter 
+		public String getCode() {
+			return code;
+		}
+		
+		public void setCode(String code) {
+			this.code = code;
+		}
 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public String getName() {
+			return name;
+		}
 
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		
+		public int getPrice() {
+			return price;
+		}
+
+		
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+
+		public int getStock() {
+			return stock;
+		}
+
+		
+		public void setStock(int stock) {
+			this.stock = stock;
+		}
+		
+		
+		@Override
+		public String toString() {
+			return "Product [code=" + code + ", name=" + name + ", price=" + price + ", stock=" + stock + "]";
+		}
+
+		
+		/*
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		}
+		*/
 	}
-
-}
